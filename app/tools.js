8f5e2acc9105bbbdb121a3b75c00e16936e4a99d
Henri Michelon <hmichelon@esimed.fr>
2020-03-13 15:25:24 +0100

    JWT support

--- app/tools.js
+++ app/tools.js
@@ -5,8 +5,12 @@ function $(selector, f) {
         document.querySelectorAll(selector).forEach(f)
 }
 
-function fetchJSON(url) {
-    return new Promise((resolve, reject) => fetch(url, {cache: "no-cache"})
+function fetchJSON(url, token) {
+    const headers = new Headers();
+    if (token !== undefined) {
+        headers.append("Authorization", `Bearer ${token}`)
+    }
+    return new Promise((resolve, reject) => fetch(url, {cache: "no-cache", headers: headers})
         .then(res => {
             if (res.status === 200) {
                 resolve(res.json())
